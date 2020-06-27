84716f10893b9d248a67c2e2c73bfdbc39cac46f
Maksym <maks.mihalchuk.2000@gmail.com>
2020-06-27 21:48:10 +0300

    Initial Commit

---
+++ 4a. Switch DZ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  4a. Switch DZ
+//
+//  Created by maks on 27.06.2020.
+//  Copyright © 2020 maks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
